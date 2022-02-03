57c8bad2e58dafbad0980c338efb05b6accb5220
wuxinnan <xinnaw2@uci.edu>
2022-02-03 12:26:45 -0800

    Clean up for the test cases.

--- src/test/java/org/jfree/chart3d/data/RangeTest.java
+++ src/test/java/org/jfree/chart3d/data/RangeTest.java
@@ -33,12 +33,9 @@
 package org.jfree.chart3d.data;
 
 import org.jfree.chart3d.TestUtils;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Some tests for the {@link Range} class.
@@ -50,12 +47,7 @@ public class RangeTest {
         Range range1 = new Range(0, 10);
         Range range2 = new Range(0, 10);
         assertEquals(range1, range2);
-		
-		// NEW TESTS ADDED BY MSWE GROUP 
-		assertThrows(IllegalArgumentException.class, () -> {new Range(5.0, 1.0);});
-		Range range3 = new Range(10, 10);
-		//assertTrue(Range.min)
-    
+
         range1 = new Range(2, 4);
         assertNotEquals(range1, range2);
         range2 = new Range(2, 4);
@@ -97,5 +89,14 @@ public class RangeTest {
         Range r1 = new Range(0, 10);
         Range r2 = (Range) TestUtils.serialized(r1);
         assertEquals(r1, r2);
+    }
+
+    // NEW TESTS ADDED BY MSWE GROUP
+    // A test making sure the Range correctly throws exception when min > max.
+    @Test
+    public void testRange(){
+        assertThrows(IllegalArgumentException.class, () -> {new Range(5.0, 1.0);});
+        assertDoesNotThrow(()->new Range(1.0, 10.0));
+        assertDoesNotThrow(()->new Range(10.0, 10.0));
     }
 }
--- src/test/java/org/jfree/chart3d/util/ArgsTest.java
+++ src/test/java/org/jfree/chart3d/util/ArgsTest.java
@@ -1,38 +1,45 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+package org.jfree.chart3d.util;
+
+import org.jfree.chart3d.data.Range;
 import org.junit.jupiter.api.Test;
 import org.jfree.chart3d.internal.Args;
-import org.jfree.chart3d.TestUtils;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
  * Some tests for the {@link Args} class.
  */
-public class RangeTest {
+public class ArgsTest {
 
     @Test
     public void testEquals() {
-        
-		// NEW TESTS ADDED BY MSWE GROUP 
-		
-		// test null
-		assertThrows(IllegalArgumentException.class, () -> {Args.nullNotPermitted(null, "color");});
-		
-		// test negative
-		assertThrows(IllegalArgumentException.class, () -> {Args.negativeNotPermitted(-5.0, "color");});
-		
-		// test positive required
-		assertThrows(IllegalArgumentException.class, () -> {Args.positiveRequired(-5.0, "color");});
-		       Range range1 = new Range(0, 10);
+
+        // NEW TESTS ADDED BY MSWE GROUP
+        // test null
+        assertThrows(IllegalArgumentException.class, () -> {
+            Args.nullNotPermitted(null, "color");
+        });
+
+        // test negative
+        assertThrows(IllegalArgumentException.class, () -> {
+            Args.negativeNotPermitted(-5.0, "color");
+        });
+
+        // test positive required
+        assertThrows(IllegalArgumentException.class, () -> {
+            Args.positiveRequired(-5.0, "color");
+        });
+        Range range1 = new Range(0, 10);
         Range range2 = new Range(0, 10);
         assertEquals(range1, range2);
-		
-		
-		
+    }
+}
+
+
+
 		// finiteRequired
-		
+
 		// finitePositiveRequired
-		
+
 		// checkArrayBounds
 		//IllegalArgumentException
