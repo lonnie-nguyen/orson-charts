7c40e1a460a7d88b9d3e14e7f6b3e5bd79a31b6b
lonnie-nguyen <50277976+lonnie-nguyen@users.noreply.github.com>
2022-02-16 12:46:27 -0800

    Add test class ExportFormatsTest

---
+++ src/test/java/org/jfree/chart3d/export/ExportFormatsTest.java
@@ -0,0 +1,27 @@
+package org.jfree.chart3d.export;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ExportFormatsTest {
+	
+	@Test
+	public void testConstructor() {
+		// Not in classpath expect False
+		ExportFormats ef = new ExportFormats();
+	}
+
+	@Test
+	public void testIsJFreeSVGAvailable() {
+		// Not in classpath expect False
+		assertFalse(ExportFormats.isJFreeSVGAvailable());
+	}
+
+	@Test
+	public void testIsJFreePDFAvailable() {
+		// Not in classpath expect False
+		assertFalse(ExportFormats.isJFreePDFAvailable());
+	}
+
+}
